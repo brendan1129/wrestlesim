f25884350a3200e35bee5b82515dd23a39f5861c
Brendan Hannum <90215602+brendan1129@users.noreply.github.com>
2024-02-21 23:29:05 -0500

    Privatizing key

--- src/main/java/com/bnh/wrestlim/beans/AWSCredentials.java
+++ src/main/java/com/bnh/wrestlim/beans/AWSCredentials.java
@@ -5,8 +5,8 @@ import com.amazonaws.auth.BasicAWSCredentials;
 
 public class AWSCredentials {
     public static final BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
-            "AKIA5FTZDQLYE3EX4JSK",
-            "6UYW/NSE8RxjNz/SIkygRbHgr/0a7t77uSX/aLEV"
+            "accessKey",
+            "secretKey"
     );
 }
 
